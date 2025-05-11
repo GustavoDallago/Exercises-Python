dd453a786d4f6716d5c01a40d2e603a157da4ec4
Gustavo Dallago Ferreira <102759388+GustavoDallago@users.noreply.github.com>
2025-05-11 20:21:30 -0300

    Delete If Else Elif directory

--- If Else Elif/Atividade 9.py
+++
@@ -1,9 +0,0 @@
-#Escreva um programa que receba um número inteiro e exiba uma mensagem 
-#informando se ele é par ou ímpar.
-
-numero = int(input("Digite um número: "))
-
-if numero % 2 == 0:
-    print(f"O número {numero} é par.")
-else:
-    print(f"O número {numero} é ímpar.")
