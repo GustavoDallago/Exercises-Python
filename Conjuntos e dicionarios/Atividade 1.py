c15dba6967abcde0a3ca9b1cb7e82288fc8dfa81
Gustavo Dallago Ferreira <102759388+GustavoDallago@users.noreply.github.com>
2025-05-11 20:21:15 -0300

    Delete Conjuntos e dicionarios directory

--- Conjuntos e dicionarios/Atividade 1.py
+++
@@ -1,8 +0,0 @@
-convidados = set()
-
-while True:
-    nome = input('Insira o nome do convidado (ou digite "sair"): ')
-    if nome.lower() == 'sair':
-        break
-    convidados.add(nome)
-print(convidados)
--- Conjuntos e dicionarios/Atividade 2.py
+++
@@ -1,7 +0,0 @@
-texto1 = set(input("Texto 1: ").lower().split())
-
-texto2 = set(input("Texto 2: ").lower().split())    
-
-comum = texto1.intersection(texto2)
-
-print(comum)
