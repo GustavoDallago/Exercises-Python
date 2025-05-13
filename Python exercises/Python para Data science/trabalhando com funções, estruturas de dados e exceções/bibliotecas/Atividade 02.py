6ef5ffa591577dc1667b7f0a48dcf5d744733e9a
Gustavo Dallago Ferreira <102759388+GustavoDallago@users.noreply.github.com>
2025-05-13 08:53:36 -0300

    Delete Python exercises/Python para Data science/trabalhando com funções, estruturas de dados e exceções/bibliotecas directory

--- Python exercises/Python para Data science/trabalhando com funções, estruturas de dados e exceções/bibliotecas/Atividade 02.py
+++
@@ -1,9 +0,0 @@
-##!pip install matplotlib==3.7.1
-
-import numpy as np
-from random  import choice, randrange
-import math
-
-#Crie um programa que sorteia, aleatoriamente, um número inteiro positivo menor que 100.
-
-random.randrange(1, 100)
